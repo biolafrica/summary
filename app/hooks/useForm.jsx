4d983a6f2827d2bf11239f3fe6d5dbf258bc9f41
Abiodun Biobaku <biolafrica@gmail.com>
2025-03-25 12:11:51 +0000

    created a useform function for form managememnt

---
+++ app/hooks/useForm.jsx
@@ -0,0 +1,20 @@
+"use client"
+
+import { useState } from "react"
+
+export default function useForm(initialValues){
+
+  const [formData, setFormData] = useState("initialValues");
+
+  const handleInputChange = (e)=>{
+    const {name, value} = e.target
+    setFormData({...formData, [name]: value})
+  }
+
+  const resetForm=()=>{
+    setFormData(initialValues)
+  }
+
+  return{formData, handleInputChange, resetForm}
+
+}
