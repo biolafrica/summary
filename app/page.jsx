30614d74b0e09c0e58005596866c10558d0d3cd5
Abiodun Biobaku <biolafrica@gmail.com>
2025-03-28 11:02:55 +0000

    fix homepage error

--- app/page.jsx
+++ app/page.jsx
@@ -1,14 +1,5 @@
 export default async function Home() {
 
-  const response = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/tweet`, {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ message: "🚀 Testing my Twitter bot!" }),
-  });
-
-  const data = await response.json();
-  console.log(data);
-
   return (
     <div className="home_cont">
       <img src="/globe.svg" alt="globe icon" style={{width: "48px", height:"48px"}} />
