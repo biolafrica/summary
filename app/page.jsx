1c895a4f1e9b505070a82f8d3f29999397fffad7
Abiodun Biobaku <biolafrica@gmail.com>
2025-03-31 14:29:07 +0100

    twitter connection  update

--- app/page.jsx
+++ app/page.jsx
@@ -1,13 +1,9 @@
-import Test from "./components/test";
-
 export default async function Home() {
 
   return (
     <div className="home_cont">
       <img src="/globe.svg" alt="globe icon" style={{width: "48px", height:"48px"}} />
-      <h2 >Welcome Home Baby</h2>
-      <Test/>
-      
+      <h2 >Welcome Home Baby</h2>      
     </div>
    
   );
